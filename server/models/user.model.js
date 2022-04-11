3c0f8a7f1ff7c7b4692f3139027492c5b6863543
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2022-04-11 10:29:13 -0700

    converted to environmental variables

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt =require("bcrypt");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -17,6 +18,12 @@ const UserSchema = new Schema({
     password: {
         type: String,
         minlength: [8, "Password must be at least 8 characters in length"],
-        
+        required: [true, "Password is required"]
+
     }
-})
+}, {timestamps:true})
+
+
+UserSchema.virtual("confirmPassword")
+    .get(()=>this._confirmPassword)
+    .set((value)=>this._confirmPassword = value)
