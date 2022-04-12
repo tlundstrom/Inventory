36a13094735edd9e48434e89446095a9ebd8c684
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2022-04-12 13:12:29 -0700

    created distributor and location schema

---
+++ server/models/distributor.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const DistributorSchema = new Schema({
+    name: {
+        type: String,
+        required:[true, "Distributor name is required."],
+        minlength:[2, "Distributor name must be at least 2 characters long."]
+    },
+    rep: {
+        type: String,
+        required: [true, "Sales rep is required"]
+    },
+    phoneNumber: {
+        type: String,
+        required:[true, "Phone number is required"]
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+    },
+    orderDays: {
+        type: String,
+        enum:[
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+        ]
+    },
+    orderCutoff: {
+        type: String
+    }
+}, {timestamps:true})
+
+module.exports = mongoose.model("Location", LocationSchema);
---
+++ server/models/location.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const LocationSchema = new Schema({
+    name: {
+        type: String,
+        required:[true, "Location name is required."],
+        minlength:[2, "Location name must be at least 2 characters long."]
+    },
+    locationTemp: {
+        type: String,
+        enum:[
+            "Dry Storage",
+            "Refrigerated",
+            "Frozen"
+        ],
+        required: [true, "Location temperature is required"]
+    },
+    inventorySchedule: {
+        type: String,
+        enum: [
+            "DNI",
+            "Monthly",
+            "Quarterly",
+            "Bi-Yearly",
+            "Yearly"
+        ],
+        required:[true, "Inventory frequency is required"]
+    }
+}, {timestamps:true})
+
+module.exports = mongoose.model("Location", LocationSchema);
