964ab1e4be28101a78568706c10d43cd7678063f
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2022-04-14 21:40:43 -0700

    refactored logout added a user dashboard

---
+++ client/src/hooks/useLogout.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+
+const useLogout = (setLoggedin) => {
+    const navigate = useNavigate();
+    
+    return
+}
+
+export default useLogout
+
