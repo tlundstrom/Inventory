3b74b45616e8fa113ac6826c3b79095a47ee7b6d
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2022-04-14 18:05:11 -0700

    reformatted login/reg with material ui

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,6 +1,17 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {Link as RouterLink} from 'react-router-dom'
 import { useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+
+const theme = createTheme();
 
 const Register = (props) =>{
     const {toggleForm} = props;
@@ -43,20 +54,78 @@ const Register = (props) =>{
     }
 
     return(
-        <>
-            <form onSubmit={handleSubmit}>
-                <label>Name:</label>
-                <input type="text" value={user.name} name="name" onChange={(e)=>{handleChange(e)}}/>
-                <label>Email:</label>
-                <input type="text" value={user.email} name="email" onChange={(e)=>{handleChange(e)}}/>
-                <label>Password:</label>
-                <input type="text" value={user.password} name="password" onChange={(e)=>{handleChange(e)}}/>
-                <label>Confirm Password:</label>
-                <input type="text" value={user.confirm} name="confirm" onChange={(e)=>{handleChange(e)}}/>
-                <input type="submit" value="Register"/>
-            </form>
-            <Link onClick={toggleForm} to="#">Already Have an Account?</Link>
-        </>
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box  
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography component="h1" variant="h5">Register</Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField 
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Name"
+                            name="name"
+                            autoComplete="name"
+                            autoFocus
+                            value={user.name}
+                            onChange={(e)=>{handleChange(e)}}
+                        />
+                        <TextField 
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            value={user.email}
+                            onChange={(e)=>{handleChange(e)}}
+                        />
+                        <TextField 
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            value={user.password}
+                            onChange={(e)=>{handleChange(e)}}
+                        />
+                        <TextField 
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="confirm"
+                            label="Confirm Password"
+                            type="password"
+                            id="confirm"
+                            value={user.confirm}
+                            onChange={(e)=>{handleChange(e)}}
+                        />
+                        <Button 
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        > 
+                            Register
+                        </Button>
+                    </Box>
+                </Box>
+            </Container>
+            <Link component={RouterLink} to="/" variant="body2" onClick={toggleForm} >{"Already Have an Account?"}</Link>
+        </ThemeProvider>
+
     )
 }
 
