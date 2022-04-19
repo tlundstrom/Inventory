f34752e3a817dccf07e4eaa97f267f9bec91139d
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2022-04-19 12:25:21 -0700

    Changed login localStorage to sessionStorage

--- client/src/contexts/UserContext.js
+++ client/src/contexts/UserContext.js
@@ -1,34 +1,26 @@
-import {createContext, useState, useEffect} from "react";
+import { createContext, useState, useEffect } from "react";
 
-
-
-const UserContext = createContext(false)
-
+const UserContext = createContext(false);
 
 const UserProvider = ({ children }) => {
-    const [auth, setAuth] = useState( ()=>{
-        const localData = JSON.parse(localStorage.getItem('auth'));
-        return localData;
-    });
+	const [auth, setAuth] = useState(() => {
+		const sessionData = JSON.parse(sessionStorage.getItem("auth"));
+		return sessionData;
+	});
 
-    const login = () => {
-        setAuth(true);
-    };
+	const login = () => {
+		setAuth(true);
+	};
 
-    const logout = () => {
-        setAuth(false);
-    };
+	const logout = () => {
+		setAuth(false);
+	};
 
-    useEffect(()=>{
-        if(auth!=null){localStorage.setItem('auth', JSON.stringify(auth))}
-    }, [auth])
-    return (
-        <UserContext.Provider value={{ auth, login, logout }}>
-            {children}
-        </UserContext.Provider>
-    );
-}
-export {
-    UserContext,
-    UserProvider
-}
+	useEffect(() => {
+		if (auth != null) {
+			sessionStorage.setItem("auth", JSON.stringify(auth));
+		}
+	}, [auth]);
+	return <UserContext.Provider value={{ auth, login, logout }}>{children}</UserContext.Provider>;
+};
+export { UserContext, UserProvider };
